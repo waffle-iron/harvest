1216e378d49ca787b59fd9a624df3a19d5dd67a5
Derek Sudduth <derek_sudduth@symantec.com>
2016-05-28 10:41:44 -0700

    templating tasks file and adding yml for travis ci

---
+++ tasks.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Task for for Invoke Lib"""
+
+from invoke import run, task
+
+
+@task
+def clean():
+    pass
+
+
+@task
+def build():
+    pass
+
+
+@task
+def deploy():
+    pass
