662a724f807d08296ecf7b8a5f9fa8c43a1db40a
Derek Sudduth <derek_sudduth@symantec.com>
2016-05-28 13:06:56 -0700

    committing more structural items

---
+++ harvest/tests/test_harvest.py
@@ -0,0 +1,11 @@
+"""Harvest functional tests."""
+
+from harvest import harvest
+import mock
+import os
+import unittest
+import sys
+
+
+class TestHarvest(unittest.TestCase):
+    pass
--- setup.py
+++ setup.py
@@ -1,20 +1,47 @@
 #!/usr/bin/env python
-from setuptools import setup, find_packages
-from harvest import __version__ as version
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
+import harvest
+import sys
+
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
+
+with open('README.rst') as readme:
+    long_description = readme.read()
 
 setup(
-    name="harvest",
-    version=version,
-
-    author="Blue October",
-    author_email="derek.sudduth@gmail.com",
-
-    description=("Harvest: the gathering"),
-    long_description=open("README.md").read(),
+    name='harvest',
+    version=harvest.__version__,
+    description=('Harvest: the gathering'),
+    long_description=long_description
+    url='https://github.com/blueoct/harvest',
+    author='Blue October',
+    author_email='derek.sudduth@gmail.com',
+    license='GNU General Public License (GPL), Version 3.0',
+    packages=['harvest'],
     classifiers=[
+        "Intended Audience :: Developers",
+        "License :: General Public License"
         "Development Status :: 5 - Production/Stable",
         "Programming Language :: Python :: 3",
         "Programming Langueage :: Python :: 3.4",
         "Programming Langueage :: Python :: 3.5",
     ],
+    tests_require=[
+        'mock>=1.0.0',
+        'pytest',
+        'pytest-cov',
+    ],
+    cmdclass={'test': PyTest}
 )
