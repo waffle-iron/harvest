f767de0048e908d2e5b50dfabeb685acf393ec58
Orbra Wortham <orbra.wortham@gmail.com>
2016-05-28 21:35:54 -0700

    fixed linting issues.

--- harvest/harvest_logging.py
+++ harvest/harvest_logging.py
@@ -6,6 +6,7 @@ http://github.com/exoscale/python-logstash-formatter
 import logging
 from logstash_formatter import LogstashFormatterV1
 
+
 def filelogger(path="./harvest.log", name=None, level=logging.info):
     file_logger = logging.getLogger(name)
     handler = logging.FileHandler(path)
@@ -15,11 +16,12 @@ def filelogger(path="./harvest.log", name=None, level=logging.info):
     file_logger.addHandler(handler)
     return file_logger
 
+
 def streamlogger(name=None, level=logging.warning):
     stream_logger = logging.getLogger(name)
     handler = logging.StreamHandler()
     formatter = LogstashFormatterV1()
     handler.setFormatter(formatter)
-    file_logger.setLevel(level)
+    stream_logger.setLevel(level)
     stream_logger.addHandler(handler)
     return stream_logger
